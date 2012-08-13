cddbf7f299f229e120ca64cd2cac617740a6c19b
Simon Richardson <stickupkid@gmail.com>
2012-08-13 21:47:21 +0100

    Adding grunt to help build the final package.

---
+++ grunt.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+	var src = {
+		main: [ 'src/main/javascript/jock/Jock.js', 
+				'src/main/javascript/jock/errors/*.js',
+				'src/main/javascript/jock/utils/*.js',
+				'src/main/javascript/jock/product/*.js',
+				'src/main/javascript/jock/option/*.js',
+				'src/main/javascript/jock/ioc/errors.js',
+				'src/main/javascript/jock/ioc/*.js',
+				'src/main/javascript/jock/aop/errors.js',
+				'src/main/javascript/jock/aop/*.js']
+	};
+	
+	grunt.initConfig({
+		lint: {
+			dest: ['bin/jock.concat.0.0.1.js']
+		},
+		
+		concat: {
+			dist: {
+				src: src.main.slice(),
+				dest: 'bin/jock.concat.0.0.1.js',
+				separator: '\n'
+			}
+		},
+		
+		min: {
+			dist: {
+				src: ['bin/jock.concat.0.0.1.js'],
+				dest: 'bin/jock.min.0.0.1.js',
+			}
+		}
+	});
+
+	grunt.registerTask('default', 'concat lint:dest min');
+};
