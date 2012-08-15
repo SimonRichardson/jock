4b0ad5633d37f20ed4dc0f1d58d9553f0641cc21
Simon Richardson <stickupkid@gmail.com>
2012-08-15 21:49:45 +0100

    Start adding code for either

---
+++ src/main/javascript/jock/either/Either.js
@@ -0,0 +1,39 @@
+jock.either = jock.either || {};
+jock.either.Either = (function () {
+    "use strict";
+
+    var Impl = function () {
+        jock.product.Product.call(this);
+    };
+    Impl.prototype = new jock.product.Product();
+    Impl.prototype.name = "Either";
+
+    var Methods = {
+        isLeft:function () {
+            throw new jock.errors.AbstractMethodError();
+        },
+        isRight:function () {
+            throw new jock.errors.AbstractMethodError();
+        },
+        fold:function () {
+
+        },
+        joinLeft:function () {
+
+        },
+        joinRight:function () {
+
+        },
+        left:function () {
+
+        },
+        right:function () {
+
+        },
+        swap:function () {
+
+        }
+    };
+
+    return jock.utils.extends(Impl, Methods);
+}).call(this);
